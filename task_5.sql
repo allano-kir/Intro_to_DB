c648639f3b270e2661419ab262bc3116fedf621c
Kirui Allan Cheruiyot <allankirui17@gmail.com>
2025-08-17 23:36:29 +0300

    Add task_5.sql to insert first row into customer table

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- task_5.sql
+-- Insert a single row into the customer table
+
+INSERT INTO customers (customer_id, name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
